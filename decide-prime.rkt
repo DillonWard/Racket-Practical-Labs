d959eae34c3e3d9fb21c986378e0bafe0095c386
Dillon <Dillonward13@gmail.com>
2018-03-30 20:53:32 +0100

    updated decide prime to use modulo

--- decide-prime.rkt
+++ decide-prime.rkt
@@ -4,10 +4,6 @@
 ; Adapted from - https://docs.racket-lang.org/guide/for.html
 ; 19/01/2018
 #lang racket
-
-; define the function (decide-prime p) where 'decide-prime' is the name of the function
-; and 'p' is the number that is passed into the function
-; the decide-prime function will return true or false
 (define (decide-prime p)
   ; defines a function 'non-divisible-by'
   (define (non-divisible-by n d)
@@ -15,15 +11,37 @@
     (cond
      ((= d 1) #true)
      ; else if the remainder of n and d is equal to 0 return false
-     (else (if(= (remainder n d) 0)
+     (else (if(= (modulo n d) 0)
           #false
           (non-divisible-by n (- d 1))))))
   ; if p is equal to 1 pass '- p 1' ((- p 1) == (p - 1)) into the non-divisible-by function 
   (if (= p 1)
-      #true
+      #false
       (non-divisible-by p (- p 1))))
 
+
+
+
+;;; ; define the function (decide-prime p) where 'decide-prime' is the name of the function
+;;; ; and 'p' is the number that is passed into the function
+;;; ; the decide-prime function will return true or false
+;;; (define (decide-prime p)
+;;;   ; defines a function 'non-divisible-by'
+;;;   (define (non-divisible-by n d)
+;;;     ; if d is equal to 1 return true ((= d 1) == (d = 1))
+;;;     (cond
+;;;      ((= d 1) #true)
+;;;      ; else if the remainder of n and d is equal to 0 return false
+;;;      (else (if(= (remainder n d) 0)
+;;;           #false
+;;;           (non-divisible-by n (- d 1))))))
+;;;   ; if p is equal to 1 pass '- p 1' ((- p 1) == (p - 1)) into the non-divisible-by function 
+;;;   (if (= p 1)
+;;;       #true
+;;;       (non-divisible-by p (- p 1))))
+
 ; iterate through a list from 0 - 10 (2)
-; pass i into the decide-prime function - ((0 - 10) ==(#f #t #t #t #f #t #f #t #f #f))
-(for/list ([i 10])
+; pass i into the decide-prime function - ((1 - 10) ==(#f #t #t #f #t #f #t #f #f #f))
+
+(for/list ([i '(1 2 3 4 5 6 7 8 9 10)])
     (decide-prime i))
